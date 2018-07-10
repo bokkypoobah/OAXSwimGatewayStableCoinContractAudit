338a5832803b4064b674cd75618cfb3fe52e498a
Tamas Herman <tamas.herman@enuma.io>
2018-07-10 17:00:31 +0800

    Pin `nodejs`, `geth` and `solc` versions via the Nix package manager

---
+++ shell.nix
@@ -0,0 +1,17 @@
+# curl -sI https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz | awk '/Location:/ {print $2}'
+with import (builtins.fetchTarball "https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-18.09pre144939.14a9ca27e69/nixexprs.tar.xz") {};
+
+let
+  nodejs = nodejs-8_x;
+  geth = (lib.getBin go-ethereum);
+
+in mkShell rec {
+  buildInputs = [
+    solc geth nodejs nodePackages_8_x.pnpm
+  ];
+
+  shellHook = ''
+    print_module_version="console.log(process.versions.modules)"
+    export npm_config_store=''${NPM_STORE_PREFIX-$HOME}/.pnpm-store-abi-$(${nodejs}/bin/node -e $print_module_version)
+    '';
+}
