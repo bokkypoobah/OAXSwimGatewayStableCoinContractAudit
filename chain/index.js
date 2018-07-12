1e6500c93b3d69855bcef5e08f147175452eba18
Tamas Herman <tamas.herman@enuma.io>
2018-07-12 18:48:08 +0800

    Remove unused ganache dev chain from the deployer utility

--- chain/index.js
+++ chain/index.js
@@ -1,82 +1,41 @@
-const fs = require('fs')
 const HDWalletProvider = require("truffle-hdwallet-provider")
 const config = require('config')
-const {Ganache, solc} = require('chain-dsl/test/helpers')
-const {Web3,wad} = require('chain-dsl')
+const {solc} = require('chain-dsl/test/helpers')
+const {Web3, wad} = require('chain-dsl')
 const deployer = require('./lib/deployerProd')
 const args = process.argv.slice(2)
-const port = 3000
-const hostname = 'localhost'
-const chainDataDir = './data'
-const outDir = './out'
 
 const truffleMnemonic = config.get('mnemonic')
 
-const log = console.log
-const mkdir = (dir) => fs.existsSync(dir) || fs.mkdirSync(dir)
+const log = console.log.bind(console)
 
-// Transplanted from
-//   https://github.com/trufflesuite/ganache-cli/blob/0203931ccbf3ae53f654c6ebde314112eb6b5117/cli.js#L155-L172
-const logAccounts = (state) => {
-    log('\nAvailable Accounts')
-    log('==================')
-
-    const accounts = state.accounts
-    const addresses = Object.keys(accounts)
+const logAccounts = (addresses) => {
+    log('\nAvailable Account addresses')
+    log('===========================')
 
     addresses.forEach((address, index) => {
         let line = "(" + index + ") " + address
-        if (state.isUnlocked(address) === false) line += " 🔒"
         log(line)
     })
 }
 
-const logMnemonic = (state) => {
+const logMnemonic = (hdWalletProvider) => {
     log('\nHD Wallet')
     log('==================')
-    log(`Mnemonic:      ${state.mnemonic}`)
-    log(`Base HD Path:  ${state.wallet_hdpath}{account_index}`)
+    log(`Mnemonic:      ${hdWalletProvider.mnemonic}`)
+    log(`Base HD Path:  ${hdWalletProvider.wallet_hdpath}{account_index}`)
 }
 
-const saveContractInterface = ({options}) => {
-    const {name, address} = options
-    fs.writeFileSync(`${outDir}/${name}.json`, JSON.stringify(options, null, 4), 'utf-8')
-    log(address, `— ${name} address`)
-}
+async function main() {
+    const nodeUrl = config.get('remoteNode')
+    const provider = new HDWalletProvider(truffleMnemonic, nodeUrl)
+    logAccounts(provider.addresses)
+    logMnemonic(provider)
+    const web3 = new Web3(provider)
 
-process.on('uncaughtException', (err) => {
-    log(err.stack)
-    process.exit(1)
-})
-
-process.on("SIGINT", () => {
-    server.close((err) => {
-        if (err) log(err.stack || err)
-        process.exit()
-    })
-})
-
-mkdir(chainDataDir)
-mkdir(outDir)
-
-const server = Ganache.server({
-    mnemonic: truffleMnemonic,
-    db_path: chainDataDir
-})
-
-server.listen(port, hostname, async (err, result) => {
-    if (err) return log(err)
-    const state = result ? result : server.provider.manager.state
-    logAccounts(state)
-    logMnemonic(state)
-
-    const web3 = new Web3(new HDWalletProvider(truffleMnemonic, config.get('remoteNode'), 5)) //options: https://api.myetherapi.com/rop
-
-    const accounts = state.accounts
-    const addresses = Object.keys(accounts)
     const [
         DEPLOYER,
-    ] = addresses
+    ] = provider.addresses
 
     const role = config.get('role')
     const collector = config.get('collector')
@@ -88,17 +47,18 @@ server.listen(port, hostname, async (err, result) => {
     log(`Balance:       ${web3.utils.fromWei(balance,'ether')}`)
     log()
 
+    function compiledContracts() {
+        return solc(__dirname, './solc-input.json')
+    }
+
     switch(args[0]) {
-        case '--no-deploy':
-            log('\nContracts are not deployed again...')
-            break;
         case '--init-contract':
             /**
              * 1) Deploy Initial Contract
              */
             const {
                 token
-            } = await deployer.initContract(solc(__dirname, './solc-input.json'), 
+            } = await deployer.initContract(compiledContracts(),
                 DEPLOYER, role.SYSTEM_ADMIN,
                 collector.TRANSFER_FEE_COLLECTOR,
                 wad(limit.MINT_LIMIT),
@@ -120,11 +80,11 @@ server.listen(port, hostname, async (err, result) => {
              */
             const {
                 gateWithFee
-            } = await deployer.gateWithFeeContract(solc(__dirname, './solc-input.json'), 
+            } = await deployer.gateWithFeeContract(compiledContracts(),
                 DEPLOYER, 
                 collector.MINT_FEE_COLLECTOR, 
                 collector.BURN_FEE_COLLECTOR, 
-                )
+            )
             break;
         case '--gatewithfee-setting':
             /**
@@ -151,11 +111,17 @@ server.listen(port, hostname, async (err, result) => {
             log('==================')
             log(await deployer.toCallData(contractName, methodName, ...process.argv.slice(5)))
             break;
+        case '--no-deploy':
         default:
             log('\nContracts are not deployed again...')
     }
+    return ''
+}
 
+main().then(result => {
+    log(result)
     process.exit(0)
+}).catch(err => {
+    console.error(err)
+    process.exit(1)
 })
-
-
